3af1f353bfc2ef506449b264c3b51c03d79b2579
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-07-02 22:03:36 -0400

    Removed old configs folder

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    "allennlp>=0.9.0",
+    "allennlp==0.9.0",
     "datasets @ git+https://gitlab.com/kb-completion/datasets.git",
-    "wandb==0.8.15",
+    "wandb==0.9.1",
+    "wandb-allennlp==0.2.0"
 ]
 
 setup(
